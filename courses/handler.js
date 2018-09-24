0effc6629e59317b2e249c2f066c7080f1fe9503
sajeeva <sa.hasith@gmail.com>
2018-09-24 15:51:51 +0530

    getAllCOurses bug fix

--- courses/handler.js
+++ courses/handler.js
@@ -1,12 +1,45 @@
 'use strict';
 
-module.exports.getAllCourses = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: `Hello, the current time is ${new Date().toTimeString()}.`,
-    }),
-  };
+const mongoose = require('mongoose');
 
-  callback(null, response);
+const config = require('../config/database');
+var Course = require('../data/models/course');
+var User = require('../data/models/user');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(config.database);
+
+mongoose.connection.on('connected', function () {
+    console.log('Connected to database '+config.database);
+});
+
+mongoose.connection.on('error', function(err) {
+    console.log('Database error ' + err);
+});
+
+module.exports.getAllCourses = (event, context, callback) => {
+  
+  console.log('getAllCourses()');
+
+  Course.find().exec().then(function(courses) {
+
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({courses}),
+    };
+
+    callback(null, response);
+
+  }).catch(function(err) {
+    console.error(err);
+
+    const response = {
+      statusCode: 500
+    };
+
+    callback(null, response);
+
+  });
+  
 };
--- data/models/user.js
+++ data/models/user.js
@@ -2,7 +2,6 @@
 
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var bcrypt = require('bcrypt');
 
 // mongoose user model
 var userSchema = new Schema({
@@ -54,34 +53,34 @@ var userSchema = new Schema({
   }
 });
 
-userSchema.pre('save', function(next){
-  var user = this;
-  if(this.isModified('password') || this.isNew){
-    bcrypt.genSalt(10, function(err, salt){
-      if(err){
-        return next(err);
-      }
-      bcrypt.hash(user.password, salt, function(err, hash){
-        if(err){
-          return next(err);
-        }
-        user.password = hash;
-        next();
-      });
-    });
-  } else {
-    return next();
-  }
-});
+// userSchema.pre('save', function(next){
+//   var user = this;
+//   if(this.isModified('password') || this.isNew){
+//     bcrypt.genSalt(10, function(err, salt){
+//       if(err){
+//         return next(err);
+//       }
+//       bcrypt.hash(user.password, salt, function(err, hash){
+//         if(err){
+//           return next(err);
+//         }
+//         user.password = hash;
+//         next();
+//       });
+//     });
+//   } else {
+//     return next();
+//   }
+// });
 
-userSchema.methods.comparePassword = function(passw, cb){
-  bcrypt.compare(passw, this.password, function(err, isMatch){
-    if(err){
-      return cb(err);
-    }
-    cb(null, isMatch);
-  });
-}
+// userSchema.methods.comparePassword = function(passw, cb){
+//   bcrypt.compare(passw, this.password, function(err, isMatch){
+//     if(err){
+//       return cb(err);
+//     }
+//     cb(null, isMatch);
+//   });
+// }
 
 var User = mongoose.model('User', userSchema);
 
